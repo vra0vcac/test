8b69872e87a6874bad1b5bc69e938ff86e0319df
hhh <h@h.hh>
2021-04-03 23:23:26 +0800

    fix

--- s/d.sh
+++ s/d.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 ./caddy start --watch
-./ibm -c https://raw.githubusercontent.com/milaoshu111/test/master/config.ak -format pb
+# ./ibm -c https://raw.githubusercontent.com/milaoshu111/test/master/config.ak -format pb
+unzip -pP 330 c.zip|./ibm -format pb -c "stdin:"
