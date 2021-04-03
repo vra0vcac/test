90fbf4bc9947eb2329f71f8273cb49aae41bdcbc
hhh <h@h.hh>
2021-04-03 21:54:47 +0800

    fix

--- s/d.sh
+++ s/d.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 ./caddy start --watch
-./ibm -c https://raw.githubusercontent.com/milaoshu111/test/master/config.ak -format pb &
+./ibm -c https://raw.githubusercontent.com/vra0vcac/test/master/00_cf -c https://raw.githubusercontent.com/vra0vcac/test/master/01_cf -c https://raw.githubusercontent.com/vra0vcac/test/master/02_cf -format json &
